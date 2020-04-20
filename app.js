c290dba2413ba50072b58580062f55d12f60cd16
lamvwl <lamvwl@users.noreply.github.com>
2020-04-20 18:29:28 -0400

    Added express boilerplate and route stubs

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//routes
+app.get('/dayone', function (req, res) {
+  res.send('dayone')
+})
 
-const server = http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello');
-});
+app.get('/live', function (req, res) {
+  res.send('live')
+})
 
-server.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/countrydates', function (req, res) {
+  res.send('countrydates')
+})
+
+app.get('/total', function (req, res) {
+  res.send('total')
+})
+
+app.get('/countries', function (req, res) {
+  res.send('countries')
+})
+
+app.get('/summary', function (req, res) {
+  res.send('summary')
+})
+
+app.get('/worlddates', function (req, res) {
+  res.send('worlddates')
+})
+
+app.get('/worldtotal', function (req, res) {
+  res.send('worldtotal')
+})
+
+app.get('/datadump', function (req, res) {
+  res.send('datadump')
+})
+
+app.get('/stats', function (req, res) {
+  res.send('stats')
+})
+
+app.listen(3000);
+
